f3e55d559de7f89e375ebb95f701efad0383e66f
gunawan2909 <gunawan29092000@gmail.com>
2023-09-01 15:36:14 +0700

    final midelware

--- routes/api.php
+++ routes/api.php
@@ -21,18 +21,21 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
-Route::get('/category/show/{category}', [CategoryController::class, 'show'])->name('category.show');
-Route::post('category/add', [CategoryController::class, 'store'])->name('category.add');
-Route::post('category/update/{category}', [CategoryController::class, 'update'])->name('category.update');
-Route::post('category/delete/{category}', [CategoryController::class, 'destroy'])->name('category.delete');
+Route::middleware(['auth:sanctum'])->group(function () {
 
 
+    Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+    Route::get('/category/show/{category}', [CategoryController::class, 'show'])->name('category.show');
+    Route::post('category/add', [CategoryController::class, 'store'])->name('category.add');
+    Route::post('category/update/{category}', [CategoryController::class, 'update'])->name('category.update');
+    Route::post('category/delete/{category}', [CategoryController::class, 'destroy'])->name('category.delete');
 
-Route::get('/product', [ProductController::class, 'index'])->name('product.index');
-Route::post('product/add', [ProductController::class, 'store'])->name('product.add');
-Route::get('product/show/{product}', [ProductController::class, 'show'])->name('product.show');
-Route::post('product/update/{product}', [ProductController::class, 'update'])->name('product.update');
-Route::post('product/delete/{product}', [ProductController::class, 'destroy'])->name('product.delete');
-Route::get('/search', [SearchController::class, 'index'])->name('search');
+
+
+    Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+    Route::post('product/add', [ProductController::class, 'store'])->name('product.add');
+    Route::get('product/show/{product}', [ProductController::class, 'show'])->name('product.show');
+    Route::post('product/update/{product}', [ProductController::class, 'update'])->name('product.update');
+    Route::post('product/delete/{product}', [ProductController::class, 'destroy'])->name('product.delete');
+    Route::get('/search', [SearchController::class, 'index'])->name('search');
+});
